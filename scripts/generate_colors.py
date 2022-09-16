f34c438d70a681ba410fe7731b3a6a5cf384f7ce
Tai <1174501146@qq.com>
2022-09-17 00:17:25 +0800

    bug fix

--- scripts/generate_colors.py
+++ scripts/generate_colors.py
@@ -8,8 +8,3 @@
 #
 # colors = [matplotlib.colors.to_hex(v, keep_alpha=False) for v in chain(*[plt.get_cmap(v).colors for v in col])]
 # print(colors)
-
-from flet_icon.app import Application
-import flet
-
-flet.app(target=Application())
--- src/flet_icon/__init__.py
+++ src/flet_icon/__init__.py
@@ -1,1 +1,1 @@
-from app import *
+from .app import *
