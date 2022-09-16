d45862f6c90ab612d6a444f531628aa6dcb5e8e0
Tai <1174501146@qq.com>
2022-09-17 00:09:34 +0800

    new license and remove import bug

--- scripts/generate_colors.py
+++ scripts/generate_colors.py
@@ -1,10 +1,15 @@
-from itertools import chain
+# from itertools import chain
+#
+# import matplotlib
+# import matplotlib.pyplot as plt
+#
+# col = [
+#     'Pastel1', 'Pastel2', "Set3"]
+#
+# colors = [matplotlib.colors.to_hex(v, keep_alpha=False) for v in chain(*[plt.get_cmap(v).colors for v in col])]
+# print(colors)
 
-import matplotlib
-import matplotlib.pyplot as plt
+from flet_icon.app import Application
+import flet
 
-col = [
-    'Pastel1', 'Pastel2', "Set3"]
-
-colors = [matplotlib.colors.to_hex(v, keep_alpha=False) for v in chain(*[plt.get_cmap(v).colors for v in col])]
-print(colors)
+flet.app(target=Application())
