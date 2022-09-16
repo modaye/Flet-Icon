867262954005911d555c03909a9fc6e4e79a6b71
Tai <1174501146@qq.com>
2022-09-16 23:30:36 +0800

    init app

---
+++ scripts/generate_colors.py
@@ -0,0 +1,10 @@
+from itertools import chain
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+col = [
+    'Pastel1', 'Pastel2', "Set3"]
+
+colors = [matplotlib.colors.to_hex(v, keep_alpha=False) for v in chain(*[plt.get_cmap(v).colors for v in col])]
+print(colors)
